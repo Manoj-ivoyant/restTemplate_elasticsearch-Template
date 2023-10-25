dd465b8fc780238a522f4eecc4e1f44c4030aa91
Manoj-ivoyant <manojmk2209@gmail.com>
2023-10-25 12:38:09 +0530

    added search functionalities using restHighlevelTemplate of elastic search

---
+++ src/main/java/com/ivoyant/resttemplateelasticsearchdemo/ResttemplateElasticsearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ivoyant.resttemplateelasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplateElasticsearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplateElasticsearchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivoyant/resttemplateelasticsearchdemo/ResttemplateElasticsearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoyant.resttemplateelasticsearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateElasticsearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
